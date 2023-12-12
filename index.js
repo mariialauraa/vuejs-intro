a1a39556d38beb61336a91939cbe3b73b8cfaea6
mariialauraa <mlauraifg@gmail.com>
2023-12-12 11:19:25 -0300

    Primeiro Contato com o Vue - API do GitHub

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 //uma instância de uma aplicação no VueJS
 const app = Vue.createApp({    
     
-    //retorna dados JS
+    //variáveis q o Vue consegue manipular dinamicamente
     data() {
         return {
-            name: 'OneBitCode',
-            count: 0
+            login: 'jpaulo',
+            name: 'João Paulo',
+            company: 'Acme Inc.',
+            bio: '...',
+            avatar_url: 'https://unsplash.it/256',
+            searchInput: '' //para começar vazio
         }
     },
+    //método que vai chamar a API do GitHub
     methods: {
-        increaseCount() {
-            //referência o 'count' do data
-            this.count++ 
+        //uma busca de forma assíncrona
+        async fetchGithubUser() {
+            const resposta = await fetch(`https://api.github.com/users/${this.searchInput}`)
+            //desestrutura a 'resposta' e pega os valores dela:
+            const { login, name, company, bio, avatar_url } = await resposta.json() //converte p/ json
+            
+            //seta os dados nas propriedades dinâmicas:
+            this.login = login
+            this.name = name
+            this.company = company
+            this.bio = bio
+            this.avatar_url = avatar_url
         }
     }
 
