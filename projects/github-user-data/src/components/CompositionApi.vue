c404fb4f324b7e488376be9d198e61e370f92123
mariialauraa <mlauraifg@gmail.com>
2023-12-13 18:12:37 -0300

    Templates e Diretivas do Vue.js

--- projects/github-user-data/src/components/CompositionApi.vue
+++ projects/github-user-data/src/components/CompositionApi.vue
@@ -2,11 +2,12 @@
 import { reactive } from 'vue';
 
 const state = reactive({
-  login: 'jpaulo',
-  name: 'João Paulo',
-  company: 'Acme Inc.',
-  bio: '...',
-  avatar_url: 'https://unsplash.it/256',
+  login: '',
+  name: '',
+  company: '',
+  bio: '',
+  avatar_url: '',
+  repos: [], //repositório
   searchInput: ''
 })
 
@@ -19,17 +20,36 @@ async function fetchGithubUser() {
   state.company = company
   state.bio = bio
   state.avatar_url = avatar_url
+
+  fetchUserRepositories(login)
+}
+
+//vai pegar os repositórios públicos do usuário
+async function fetchUserRepositories() {
+  const resposta = await fetch(`https://api.github.com/users/${state.login}/repos`)
+  const repos = await resposta.json()
+
+  state.repos = repos
 }
 </script>
 
-
-<template>
+<template>  
   <h2>GitHub User Data</h2>
   <input type="text" v-model="state.searchInput">
   <button v-on:click="fetchGithubUser">Carregar Usuário</button>
-  <img v-bind:src="state.avatar_url">
-  <strong>@{{ state.login }}</strong>
-  <h1>{{ state.name }}</h1>
-  <h3>{{ state.company }}</h3>
-  <span>{{ state.bio }}</span>
+  <div v-if="state.login !== ''">
+    <img v-bind:src="state.avatar_url">
+    <strong>@{{ state.login }}</strong>
+    <h1>{{ state.name }}</h1>
+    <h3>{{ state.company }}</h3>
+    <span>{{ state.bio }}</span>
+  </div>
+
+  <section v-if="state.repos.length > 0">
+    <article v-for="repo of state.repos">
+      <h3>{{ repo.full_name }}</h3>
+      <p>{{ repo.description }}</p>
+      <a :href="repo.html_url" target="_blank">Ver no GitHub</a>
+    </article>
+  </section>
 </template>
